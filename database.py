9f7bab5de17c4ee4a620332e2744d9fa7d864405
NirGorin <nirgorin@gmail.com>
2025-07-18 14:00:00 +0300

    Set up SQLAlchemy connection with SQLite3

---
+++ database.py
@@ -0,0 +1,10 @@
+#database file:
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
+
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./MorizApp.db'
+# SQLALCHEMY_DATABASE_URL='postgresql://postgres:Nirgo0rin@localhost/MorizApplicationDatabase'
+engine=create_engine(SQLALCHEMY_DATABASE_URL)
+SessionMoriz=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+Base=declarative_base()
