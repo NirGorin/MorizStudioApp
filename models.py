a506e86b117153c2bbba007eadb152ddd38237b8
NirGorin <nirgorin@gmail.com>
2025-07-19 09:00:00 +0300

    Add Trainer and Limitation models with many-to-many relationship

---
+++ models.py
@@ -0,0 +1,35 @@
+
+#models file:
+
+from sqlalchemy import Column, String, Integer, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+from MorizApp.database import Base
+
+
+
+trainer_limitation= Table('trainer_limitation', Base.metadata,
+                          Column('trainer_id', Integer, ForeignKey('trainers.ID')),
+                          Column('limitation_id', Integer, ForeignKey('limitations.ID'))
+                         )
+
+
+class Trainer(Base):
+    __tablename__='trainers'
+    ID=Column(Integer,primary_key=True,index=True)
+    First_Name=Column(String(100))
+    Last_Name=Column(String(100))
+    username=Column(String(100),unique=True)
+    Email=Column(String(100),unique=True)
+    Password=Column(String(100))
+    Level=Column(String(100))
+    Number_Of_Training=Column(Integer)
+    Role=Column(String(100))
+    Phone_Number=Column(String(20),unique=True)
+    Limitations=relationship("Limitation",secondary=trainer_limitation,back_populates="Trainers")
+
+class Limitation(Base):
+    __tablename__='limitations'
+    ID=Column(Integer,primary_key=True,index=True)
+    limitation = Column(String(100))
+    Trainers=relationship("Trainer",secondary=trainer_limitation,back_populates="Limitations")
