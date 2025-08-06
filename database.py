de2c03f2bfa38a79e77f8138f548d767e3890c1e
NirGorin <nirgorin@gmail.com>
2025-08-06 11:29:51 +0300

    update database file

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./MorizApp.db'
-# SQLALCHEMY_DATABASE_URL='postgresql://postgres:Nirgo0rin@localhost/MorizApplicationDatabase'
+
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
 SessionMoriz=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
