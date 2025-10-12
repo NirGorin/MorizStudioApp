367be4c0372db5b3945d35678d627c26e4bc684e
NirGorin <nirgorin@gmail.com>
2025-10-12 17:38:53 +0300

    feat(routers): complete & update CRUDS for studios/users/trainee_profile

--- app/routers/trainee_profile.py
+++ app/routers/trainee_profile.py
@@ -2,11 +2,11 @@
 from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks, status
 from sqlalchemy.orm import Session
 import json, os
-from ..models import TraineeProfile
+from ..models import TraineeProfile, User
 from ..services.ai_service import process_profile_in_background
 from ..schemas import CreateTraineeProfileRequest
-from ..routers.auth import get_current_user
-from .auth import get_db                
+from .auth import get_db,get_current_user,db_dependecy
+from .users import user_dependecy
 from redis.asyncio import Redis
 
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
@@ -15,30 +15,24 @@ CACHE_TTL_SECONDS = 3600
 
 router = APIRouter(prefix="/trainee_profile", tags=["trainee_profile"])
 
-@router.delete("/delete_trainee_profile/{profile_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_trainee_profile(
-    profile_id: int,
-    db: Session = Depends(get_db),           
-    user: dict = Depends(get_current_user),
-):
-    if user['role'] == 'trainer':
-        raise HTTPException(status_code=403, detail="User role not allowed to delete trainee profile")
-    profile = db.query(TraineeProfile).filter(TraineeProfile.id == profile_id).first()
-    if not profile:
-        raise HTTPException(status_code=404, detail="Trainee profile not found")
-    db.delete(profile)
-    db.commit()
-    return {"message": "Trainee profile deleted successfully"}
+
 
 @router.post("/create_trainee_profile", status_code=status.HTTP_201_CREATED)
 async def create_trainee_profile(
-    profile: CreateTraineeProfileRequest,    
+    profile: CreateTraineeProfileRequest,
     background_tasks: BackgroundTasks,
-    db: Session = Depends(get_db),           
-    user: dict = Depends(get_current_user),
+    db: db_dependecy,
+    user: user_dependecy
 ):
-    if user['role'] == 'trainer':
+    if not user:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+
+    if user.get("role") == "trainer":
         raise HTTPException(status_code=403, detail="User role not allowed to create trainee profile")
+
+    existing = db.query(TraineeProfile).filter(TraineeProfile.user_id == user.get("id")).first()
+    if existing:
+        raise HTTPException(status_code=400, detail="Trainee profile already exists for this user")
 
     trainee_profile = TraineeProfile(
         age=profile.Age,
@@ -48,7 +42,7 @@ async def create_trainee_profile(
         level=profile.Level,
         number_of_week_training=profile.Number_Of_Week_Training,
         limitations=profile.Limitation,
-        user_id=user['id'],
+        user_id=user.get("id"),       
         ai_status="queued",
     )
     db.add(trainee_profile)
@@ -58,7 +52,7 @@ async def create_trainee_profile(
     cache_key = f"trainee:{trainee_profile.id}:profile"
     cache_payload = {
         "profile_id": trainee_profile.id,
-        "user_id": user["id"],
+        "user_id": user.get("id"),
         "age": profile.Age,
         "gender": profile.Gender,
         "height_cm": profile.Height,
@@ -74,10 +68,16 @@ async def create_trainee_profile(
         pass
 
     background_tasks.add_task(process_profile_in_background, trainee_profile.id)
+
     return {"message": "Trainee profile created successfully", "profile_id": trainee_profile.id}
 
+
 @router.get("/trainees/{profile_id}/profile_cache", status_code=status.HTTP_200_OK)
-async def get_trainee_profile_cache_only(profile_id: int):
+async def get_trainee_profile_cache_only(profile_id: int,db: db_dependecy,
+                                         user: user_dependecy):
+    user_model = db.query(User).filter(User.id == user.get("id")).first()
+    if not user_model or user_model.id != user.get('id'):
+        raise HTTPException(status_code=403, detail="Not authorized to access this trainee profile cache")
     cache_key = f"trainee:{profile_id}:profile"
     try:
         raw = await r.get(cache_key)
@@ -94,8 +94,18 @@ async def get_trainee_profile_cache_only(profile_id: int):
 @router.get("/trainees/{profile_id}/profile", status_code=status.HTTP_200_OK)
 async def get_trainee_profile(
     profile_id: int,
-    db: Session = Depends(get_db),           
+    db: db_dependecy,
+    user: user_dependecy           
 ):
+    if not user:
+        raise HTTPException(status_code=403, detail="Not authorized to access trainee profile")
+    tp = db.query(TraineeProfile).filter(TraineeProfile.id == profile_id).first()
+    if not tp:
+        raise HTTPException(status_code=404, detail="Trainee profile not found")
+    if user.get("id") != tp.user_id or user.get("role") == "trainer":
+        raise HTTPException(status_code=403, detail="Not allowed to access this trainee profile")
+
+    
     cache_key = f"trainee:{profile_id}:profile"
     try:
         raw = await r.get(cache_key)
@@ -108,10 +118,7 @@ async def get_trainee_profile(
             data = {"raw": raw}
         return {"profile_id": profile_id, "cache": "HIT", "data": data}
 
-    tp = db.query(TraineeProfile).filter(TraineeProfile.id == profile_id).first()
-    if not tp:
-        raise HTTPException(status_code=404, detail="Trainee profile not found")
-
+   
     payload = {
         "profile_id": tp.id,
         "user_id": tp.user_id,
@@ -123,6 +130,8 @@ async def get_trainee_profile(
         "number_of_week_training": tp.number_of_week_training,
         "limitations": tp.limitations,
         "ai_status": tp.ai_status,
+        "ai_summary": tp.ai_summary,
+        "ai_json": tp.ai_json,
     }
     try:
         await r.set(cache_key, json.dumps(payload), ex=CACHE_TTL_SECONDS)
@@ -134,10 +143,10 @@ async def get_trainee_profile(
 async def update_trainee_profile(
     profile_id: int,
     profile: CreateTraineeProfileRequest,    
-    db: Session = Depends(get_db),           
-    user: dict = Depends(get_current_user),
+    db: db_dependecy,           
+    user: user_dependecy,
 ):
-    if user['role'] == 'trainer':
+    if user.get('role') == 'trainer':
         raise HTTPException(status_code=403, detail="User role not allowed to update trainee profile")
     profile_model = db.query(TraineeProfile).filter(TraineeProfile.id == profile_id).first()
     if not profile_model:
@@ -172,4 +181,21 @@ async def update_trainee_profile(
     except Exception:
         pass
     return {"message": "Trainee profile updated successfully", "profile_id": profile_model.id}
+
+@router.delete("/delete_trainee_profile/{profile_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_trainee_profile(
+    profile_id: int,
+    db: db_dependecy,           
+    user: user_dependecy
+):
+    if not user:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+    if user.get('role') == "trainer" :
+        raise HTTPException(status_code=403, detail="Not allowed to delete trainee profile")
+    profile = db.query(TraineeProfile).filter(TraineeProfile.id == profile_id).first()
+    if not profile or not user.get('role') == "admin":
+        raise HTTPException(status_code=404, detail="Trainee profile not found")
+    db.delete(profile)
+    db.commit()
+    return {"message": "Trainee profile deleted successfully"}
     
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,14 +1,16 @@
 # app/routers/users.py
 import os
+from typing import Annotated, List
 import redis
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
-from schemas import CreateUserRequest
-from .auth import get_db
+from passlib.context import CryptContext
+from app.schemas import CreateUserRequest, UpdatePasswordRequest, updateUsernameRequest,UserResponse,UpdateUserRequest
+from .auth import get_db, get_current_user, db_dependecy, bcryptcontext
 from ..models import User  
 
 router = APIRouter(prefix="/users", tags=["users"])
+user_dependecy = Annotated[dict,Depends(get_current_user)]
 
 
 r = redis.Redis.from_url(os.getenv("REDIS_URL"), decode_responses=True)
@@ -25,15 +27,23 @@ r = redis.Redis.from_url(
 
 CACHE_TTL_SECONDS = 600
 
-@router.get("/{user_id}/",status_code=200)
-def get_user(user_id: int, db: Session = Depends(get_db)):
+@router.get("/{user_id}/",status_code=200,response_model=UserResponse)
+def get_user(user_id: int, db: db_dependecy,user: user_dependecy):
+    if not user:
+        raise HTTPException(status_code=403, detail="Not authorized to access user information")
     user_model = db.query(User).filter(User.id == user_id).first()
     if not user_model:
         raise HTTPException(status_code=404, detail="User not found")
+    if user.get("id") != user_id:
+        raise HTTPException(status_code=403, detail="Not authorized to access this user's information")
     return user_model
 
 @router.get("/{user_id}/email")
-def get_user_email(user_id: int, db: Session = Depends(get_db)):
+def get_user_email(user_id: int, db: db_dependecy, user: user_dependecy):
+    
+    if not user:
+        raise HTTPException(status_code=403, detail="Not authorized to access user email")
+    
     key = f"user:{user_id}:email"
 
     
@@ -52,18 +62,24 @@ def get_user_email(user_id: int, db: Session = Depends(get_db)):
     
     return {"user_id": user_id, "email": email, "cache": "MISS"}
 
-@router.delete("/{user_id}/")
-def delete_user_email(username: str, db: Session = Depends(get_db)):
+@router.delete("/{username}/")
+def delete_user_by_username(username: str, db: db_dependecy, user: user_dependecy):
+    if not user:
+        raise HTTPException(status_code=403, detail="Not authorized to delete user")
     user_model = db.query(User).filter(User.username == username).first()
     if not user_model:
         raise HTTPException(status_code=404, detail="User not found")
+    if user_model.id != user.get('id'):
+        raise HTTPException(status_code=403, detail="Not authorized to delete this user")
     db.delete(user_model)
     db.commit()
     return {"message": "User deleted successfully"}
 
 
 @router.put("/{user_id}/update_user")
-def update_user(user_id: int, user: CreateUserRequest, db: Session = Depends(get_db)):
+def update_user(user_id: int, user: UpdateUserRequest, db: db_dependecy, user_check: user_dependecy):
+    if  not user_check:
+        raise HTTPException(status_code=403, detail="Not authorized to update user")
     user_model = db.query(User).filter(User.id == user_id).first()
     if not user_model:
         raise HTTPException(status_code=404, detail="User not found")
@@ -71,6 +87,7 @@ def update_user(user_id: int, user: CreateUserRequest, db: Session = Depends(get
     user_model.last_name = user.Last_Name
     user_model.email = user.Email
     user_model.phone_number = user.Phone_Number
+    user_model.role=user.Role
     db.commit()
     db.refresh(user_model)
     
@@ -80,3 +97,46 @@ def update_user(user_id: int, user: CreateUserRequest, db: Session = Depends(get
         r.setex(key, CACHE_TTL_SECONDS, user.Email)
 
     return {"message": "User updated successfully", "user": user_model} 
+
+  
+
+@router.put("/{user_id}/update_password")
+def update_password(
+    user_id: int,
+    db: db_dependecy,
+    user: user_dependecy,
+    password_request: UpdatePasswordRequest
+):
+    if not user:
+        raise HTTPException(status_code=403, detail="Not authorized to update password")
+    user_model = db.query(User).filter(User.id == user_id).first()
+    if not user_model:
+        raise HTTPException(status_code=404, detail="User not found")
+    if user_model.id != user.get('id'):
+        raise HTTPException(status_code=403, detail="Not authorized to update this user's password")
+    if not bcryptcontext.verify(password_request.Password, user_model.hashed_password):
+        raise HTTPException(status_code=400, detail="Current password is incorrect")
+    user_model.hashed_password = bcryptcontext.hash(password_request.Newpassword)
+    db.commit()
+    return {"message": "Password updated successfully"}
+
+@router.put("/{user_id}/update_username")
+def update_username(
+    user_id: int,
+    db: db_dependecy,
+    user: user_dependecy,
+    username_request: updateUsernameRequest
+):
+    if not user:
+        raise HTTPException(status_code=403, detail="Not authorized to update username")
+    user_model = db.query(User).filter(User.id == user_id).first()
+    if not user_model:
+        raise HTTPException(status_code=404, detail="User not found")
+    if user_model.id != user.get('id'):
+        raise HTTPException(status_code=403, detail="Not authorized to update this user's username")
+    existing_user = db.query(User).filter(User.username == username_request.Newusername).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username already taken")
+    user_model.username = username_request.Newusername
+    db.commit()
+    return {"message": "Username updated successfully"}
